d8802e915e2f24811d5e401d4002ac06c9ab4c18
laurenfahey24 <59398628+laurenfahey24@users.noreply.github.com>
2021-01-20 19:37:02 -0800

    Add files via upload

---
+++ interval_io.py
@@ -0,0 +1,12 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1 = input('Enter first number: ')
+Enter first number: 1.5
+>>> num2 = input('Enter second numer: ')
+Enter second numer: 6.3
+>>> sum = float(num1) + float(num2)
+>>> print('The sum of {0} and {1} is {2}'
+      The sum of 1.5 and 6.3 is 7.8
+      
+SyntaxError: invalid syntax
+>>> 
---
+++ prime_io.py
@@ -0,0 +1,15 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num = 407
+>>> if num > 1:
+	for i in range(2,num):
+		if (num % i) == ():
+			print(num,"is not a prime number")
+			print(i,"times",num//i,"is",num)
+			break
+		else:
+			print(num,"is a prime number")
+			else:
+				
+SyntaxError: invalid syntax
+>>> 
